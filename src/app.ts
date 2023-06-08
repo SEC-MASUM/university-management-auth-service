cfbf154057215e6afbf1a861b1865935f471d358
SEC-MASUM <masum.jess42@gmail.com>
2023-06-08 17:37:05 +0600

    Create User Service, Controller and Route, generateUserId utility Function

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express'
+import userRouter from './app/modules/user/user.route'
 import cors from 'cors'
 const app: Application = express()
 
@@ -8,8 +9,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application Routes
+app.use('/api/v1/user/', userRouter)
+
 //Testing
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully!')
 })
 
