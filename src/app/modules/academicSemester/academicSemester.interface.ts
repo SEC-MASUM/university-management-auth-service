b60d0dd9d58f3583374e0026f3e7fab51c3abb14
SEC-MASUM <masum.jess42@gmail.com>
2023-06-17 00:33:31 +0600

    refactor academicSemester interface model and validation using constant

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -1,11 +1,29 @@
 import { Model } from 'mongoose';
 
+export type IAcademicSemesterMonth =
+  | 'January'
+  | 'February'
+  | 'March'
+  | 'April'
+  | 'May'
+  | 'June'
+  | 'July'
+  | 'August'
+  | 'September'
+  | 'October'
+  | 'November'
+  | 'December';
+
+export type IAcademicSemesterTitle = 'Autumn' | 'Summer' | 'Fall';
+
+export type IAcademicSemesterCode = '01' | '02' | '03';
+
 export type IAcademicSemester = {
-  title: string;
+  title: IAcademicSemesterTitle;
   year: number;
-  code: string;
-  startMonth: string;
-  endMonth: string;
+  code: IAcademicSemesterCode;
+  startMonth: IAcademicSemesterMonth;
+  endMonth: IAcademicSemesterMonth;
 };
 
 export type AcademicSemesterModel = Model<IAcademicSemester>;
