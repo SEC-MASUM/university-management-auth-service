e1b4e1d97dcdfe59c1e7f4f753e1d89f70693d13
SEC-MASUM <masum.jess42@gmail.com>
2023-06-16 22:45:24 +0600

    install Zod, and handleZodError in  globalErrorHandler

---
+++ src/errors/handleZodError.ts
@@ -0,0 +1,20 @@
+import { ZodError, ZodIssue } from 'zod';
+import { IGenericErrorMessage } from '../app/interfaces/genericErrorMessage';
+import { IGenericErrorResponse } from '../app/interfaces/genericErrorResponse';
+
+const handleZodError = (error: ZodError): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = error.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue?.message,
+    };
+  });
+  const statusCode = 400;
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorMessages: errors,
+  };
+};
+
+export default handleZodError;
