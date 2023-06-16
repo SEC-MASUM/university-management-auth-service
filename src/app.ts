3f3c1437fc83a1ae2f48c0bf1b7a97a1f2cec767
SEC-MASUM <masum.jess42@gmail.com>
2023-06-17 00:58:51 +0600

    Create Controller Route and Service to create-semester

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
 import { UserRoutes } from './app/modules/user/user.route';
 const app: Application = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Application Routes
 app.use('/api/v1/user/', UserRoutes);
+app.use('/api/v1/academic-semester/', AcademicSemesterRoutes);
 
 //Testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
